bcfcd8e2394ab24371e3998ee66ef2ba3dabd583
cloudera <cloudera@quickstart.cloudera>
2015-07-14 16:49:27 -0700

    parameterized everything and remove the need for hive-site.xml

--- simpleRequest.sh
+++ simpleRequest.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-RETURN_STATUS=$(curl -I quickstart.cloudera:8000 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+# simple rest call
+# has one parameter -> the server:port to call
+
+RETURN_STATUS=$(curl -I $1 2>/dev/null | head -n 1 | cut -d$' ' -f2)
 
 if [ $RETURN_STATUS -eq "200" ] 2> /dev/null; then
 exit 0;
